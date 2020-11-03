04b64c008e7d350877b4e64e3ea89dfe2d1ab880
Jamy <jamyarrahman82@gmail.com>
2020-11-03 11:18:35 +0600

    profile-dash.php added

---
+++ LAB_TASK_4.1_SESSION/profile_dash.php
@@ -0,0 +1,38 @@
+<?php
+include_once('header.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <table align="center" width='50%' border="1">
+        <tr>
+            <td>
+                <h3>Account</h3>
+                <hr>
+                <ul>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">View Profile</a></li>
+                    <li><a href="">Edit Profile</a></li>
+                    <li><a href="">Change Profile Picture</a></li>
+                    <li><a href="">Change Password</a></li>
+                    <li><a href="">Log Out</a></li>
+                </ul>                
+            </td>
+            <td>
+                <h1>Welcome Bob</h1>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+
+<?php
+
+    include_once('footer.html');
+?>
