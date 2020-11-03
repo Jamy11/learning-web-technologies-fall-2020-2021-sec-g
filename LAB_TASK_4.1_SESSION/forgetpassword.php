ce30426e5533a2d324f0474d72355478332c07aa
Jamy <jamyarrahman82@gmail.com>
2020-11-03 10:46:21 +0600

    forgetpassword.php added

---
+++ LAB_TASK_4.1_SESSION/forgetpassword.php
@@ -0,0 +1,36 @@
+<?php
+include_once('header.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forget Password</title>
+</head>
+<body >
+    <form action="">
+        <table align="center" >
+            <tr>
+                <td>Enter Email</td>
+                <td><input type="text" name="fgemail"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <hr>
+                    <input type="submit" name='submit'> 
+                </td>
+            </tr>
+        </table>
+        
+    </form>
+    
+</body>
+</html>
+
+
+<?php
+
+    include_once('footer.html');
+?>
