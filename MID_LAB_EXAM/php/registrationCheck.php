ab91990c646b594efd37d9943c9a6dc1f9499000
Jamy <jamyarrahman82@gmail.com>
2020-11-10 09:25:36 +0600

    php added

--- MID_LAB_EXAM/php/registrationCheck.php
+++ MID_LAB_EXAM/php/registrationCheck.php
@@ -1,19 +1,64 @@
 <?php
+    session_start();
 
-    /*	$myfile = fopen('test.txt', 'w');
-	fwrite($myfile, 'this is file i/o test...');
-	fclose($myfile);
-	echo "done";*/
+    if(isset($_POST['submit']))
+    {
 
-	//$myfile = fopen('log.txt', 'r');
-	//$data = fread($myfile, filesize('test.txt'));
-	//$data = fgets($myfile);
-	// /echo $data;
-    //$myfile .= "John Smith\n";
+        if(!empty($_POST['id']))
+        {
+            $id = $_POST['id'];
+        }
+        else
+        {
+            $id ='';
+        }
+        if(!empty($_POST['password'] && !empty($_POST['con_pas'])))
+        {
+            if($_POST['password'] == $_POST['con_pas'] )
+            {
+                $pas = $_POST['password'];
+            }
+            else
+            {
+                $pas ='';
+            }
+        }
 
-	//while($data = fgets($myfile)){
-	//	echo $data . "<br>";
-    //}
+        if(!empty($_POST['name']))
+        {
+            $name = $_POST['name'];
+        }
+        else
+        {
+            $name='';
+        }
+
+        if(!empty($_POST['user_t']))
+        {
+            $user = $_POST['user_t'];
+        }
+        else
+        {
+            $user='';
+        }
+
+        if($id != '' && $pas !='' && $name!='' && $user !='' )
+        {
+            $_SESSION['id'] = $id;
+            $_SESSION['pas'] = $pas;
+            $_SESSION['name'] = $name;
+            $_SESSION['user'] =$user;
+        }
+        else
+        {
+
+        }
+
+    }
+    else
+    {
+
+    }
     
 
     //fclose($myfile);
