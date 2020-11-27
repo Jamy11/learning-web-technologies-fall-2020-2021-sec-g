e8919e801b4f97aad6140ebee2408950708b820c
Jamy <jamyarrahman82@gmail.com>
2020-11-27 09:04:41 +0600

    testing

---
+++ TEST_PHP/db conn/db/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tset";
+
+function getConnection()
+{
+	global $servername;
+	global $username;
+	global $password;
+	global $dbname;
+}
+
+?>
