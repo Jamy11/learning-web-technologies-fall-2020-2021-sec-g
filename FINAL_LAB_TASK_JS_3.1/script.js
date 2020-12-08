77560e0f55781851fd41fc3bd84300cd93e6a0b0
Jamy <jamyarrahman82@gmail.com>
2020-12-08 09:38:21 +0600

    all js function done

--- FINAL_LAB_TASK_JS_3.1/script.js
+++ FINAL_LAB_TASK_JS_3.1/script.js
@@ -1,10 +1,130 @@
-function checkAll()
+function formvalidation()
 {
+    let name = checkName();
+    let email = checkEmail();
+    let gender = checkGender();
+    let dob = checkDob();
+    let blood = checkBlood();
+    let degree = checkDegree();
+
+    if(name == true && email == true && gender == true && dob == true && blood == true && degree == true)
+    {
+        alert('All done');
+    }
+    else
+    {
+        alert('Try again');
+    }
 
 }
 
 function checkName()
 {
     let obj = document.getElementById('name').value;
-    
+    if(obj != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkEmail()
+{
+    let obj = document.getElementById('email').value;
+    if(obj != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkGender()
+{
+    let obj1 = document.getElementById('gender1').value;
+    let obj2 = document.getElementById('gender2').value;
+    let obj3 = document.getElementById('gender3').value;
+    let gen;
+
+    if (obj1 !='')
+    {
+        gen =obj1;
+    }
+
+    if (obj2 !='')
+    {
+        gen =obj2;
+    }
+
+    if (obj3 !='')
+    {
+        gen =obj3;
+    }
+
+
+    if(gen != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkDob()
+{
+    let obj = document.getElementById('dob').value;
+    if(obj != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkBlood()
+{
+    let obj = document.getElementById('blood').value;
+    if(obj != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkDegree()
+{
+    let obj1 = document.getElementById('deg1').value;
+    let obj2 = document.getElementById('deg2').value;
+    let obj3 = document.getElementById('deg3').value;
+    let deg;
+
+    if (obj1 !='')
+    {
+        deg += obj1;
+    }
+
+    if (obj2 !='')
+    {
+        deg +=obj2;
+    }
+
+    if (obj3 !='')
+    {
+        deg +=obj3;
+    }
+
+    if(deg != '')
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
